d9a45846e398cbd178d1e6bfa456d1769d8d97bc
Simon Duggal <simondu@stud.ntnu.no>
2021-12-11 14:07:00 +0100

    Added external controller class

---
+++ src/main/java/no/ntnu/librarybackend/Controller.java
@@ -0,0 +1,13 @@
+package no.ntnu.librarybackend;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+	@GetMapping("/")
+	public String getMessage(){
+		return "HELLO WORLD";
+	}
+}
--- src/main/java/no/ntnu/librarybackend/LibraryBackendApplication.java
+++ src/main/java/no/ntnu/librarybackend/LibraryBackendApplication.java
@@ -6,13 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class LibraryBackendApplication {
 
-	@GetMapping("/")
-	public String getMessage(){
-		return "HELLO WORLD";
-	}
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(LibraryBackendApplication.class, args);
